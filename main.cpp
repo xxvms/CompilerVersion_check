8dd300d7685d4a9402121ab505fb3c6a50f1e573
Tom Breza <tom@pcservicegroup.co.uk>
2017-06-12 20:37:54 +0100

    short entry how to thest for GCC version

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "gcc version: " << __GNUC__ << '.' << __GNUC_MINOR__ << '.' << __GNUC_PATCHLEVEL__ << '\n';
+
+
+    system("pause");
+    return 0;
+}
